f342ab3e7fc6bd69393d22b8a1f300cfbdb865f3
Eldon <eldon@eldondev.com>
2015-04-26 20:38:55 -0400

    Use command line args instead and do proxying

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
@@ -17,8 +18,7 @@ func main() {
 		Certificates: []tls.Certificate{cert},
 	}
 	config.Rand = rand.Reader
-	service := "0.0.0.0:443"
-	listener, err := tls.Listen("tcp", service, &config)
+	listener, err := tls.Listen("tcp", os.Args[1], &config)
 	if err != nil {
 		log.Fatalf("server: listen: %s", err)
 	}
@@ -38,27 +38,8 @@ func main() {
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 512)
-	for {
-		log.Print("server: conn: waiting")
-		n, err := conn.Read(buf)
-		if err != nil {
-			if err != nil {
-				log.Printf("server: conn: read: %s", err)
-			}
-			break
-		}
-
-		log.Printf("server: conn: echo %q\n", string(buf[:n]))
-		n, err = conn.Write(buf[:n])
-
-		n, err = conn.Write(buf[:n])
-		log.Printf("server: conn: wrote %d bytes", n)
-
-		if err != nil {
-			log.Printf("server: write: %s", err)
-			break
-		}
-	}
-	log.Println("server: conn: closed")
+	unwrapped := net.Dial("tcp", os.Args[1])
+	defer unwrapped.Close()
+	go io.Copy(conn, unwrapped)
+	io.Copy(unwrapped, conn)
 }
